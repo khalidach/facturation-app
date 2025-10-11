d654d482b7e5f44217ad732745a2ebb7e9126e8f
khalidach <khalidplaysg@gmail.com>
2025-10-11 18:42:24 +0100

    third

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,38 @@
-import { defineConfig } from "vite";
+import { resolve } from "path";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": "/src/frontend",
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: "src/main.js",
+      },
     },
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        input: "src/backend/preload.js",
+      },
+    },
+  },
+  renderer: {
+    root: "src",
+    build: {
+      rollupOptions: {
+        input: "src/index.html",
+      },
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src/frontend"),
+      },
+    },
+    plugins: [react(), tsconfigPaths()],
   },
 });
