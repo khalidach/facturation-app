3ea091a1575cb4ae42498897919d30b1653f0e35
khalidach <khalidplaysg@gmail.com>
2026-01-30 15:48:46 +0100

    fix quit the app

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ app.whenReady().then(() => {
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
